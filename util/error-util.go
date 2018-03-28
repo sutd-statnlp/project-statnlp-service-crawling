6229a713206e089b31a35f032eb80b16f5f1ed5d
Ai Nguyen <ainguyenkaka@gmail.com>
2018-03-28 09:38:18 +0800

    add getCrawlingChannel endpoint

--- util/error-util.go
+++ util/error-util.go
@@ -2,3 +2,9 @@ package util
 
 // InvalidFormData .
 const InvalidFormData = "Invalid form data !"
+
+// InvalidParam .
+const InvalidParam = "Invalid param !"
+
+// InvalidQuery .
+const InvalidQuery = "Invalid query !"
